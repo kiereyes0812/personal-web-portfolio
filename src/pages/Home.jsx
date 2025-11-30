855ff957d2bce3e856c78238370c54a0e52f8c91
[kiereyes0812] <[reyeskierbrian]>
2025-11-30 22:19:29 +0800

    Added navbar and hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,26 @@
 import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "@/components/StarBackground";
+import { Navbar } from "../components/Navbar";
+import { HeroSection } from "../components/HeroSection";
 
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-
       {/* Them Toggle */}
       <ThemeToggle />
 
       {/* Background Effect */}
+      <StarBackground />
 
       {/* Navbar */}
+      <Navbar />
 
       {/* Main Content */}
+      <main>
+        <HeroSection />
+      </main>
 
       {/* Footer */}
-
     </div>
   );
 };
